9770cfcdc2b36120ac6a382163dee133bb36a05b
brap <brap@brap.com>
2018-08-07 16:38:56 +0100

    0.13 refactored stageData constructor to pass in goaldata object in constructor, changed associated data

--- app/src/main/java/com/example/android/devmap/data/RoadMapData.java
+++ app/src/main/java/com/example/android/devmap/data/RoadMapData.java
@@ -15,21 +15,35 @@ public class RoadMapData {
         GoalsData goal1 = new GoalsData("learn Git Commands", true);
         GoalsData goal2 = new GoalsData("Learn how to use branches", false);
         GoalsData goal3 = new GoalsData("Learn to collaborate on Github", false);
+        ArrayList<GoalsData> stagegoals1 = new ArrayList<>();
+        stagegoals1.add(goal1);
+        stagegoals1.add(goal2);
+        stagegoals1.add(goal3);
         // dummy data for stage 2
         GoalsData goal4 = new GoalsData("Learn how to set up activities", false);
         GoalsData goal5 = new GoalsData("Learn how to use intents", false);
         GoalsData goal6 = new GoalsData("Learn how to send extra data in intents", false);
         GoalsData goal7 = new GoalsData("Learn how to issue implicit intents", false);
+        ArrayList<GoalsData> stagegoals2 = new ArrayList<>();
+        stagegoals2.add(goal4);
+        stagegoals2.add(goal5);
+        stagegoals2.add(goal6);
         //dummy data for stage 3
         GoalsData goal8 = new GoalsData("Learn what notifications are", false);
         GoalsData goal9 = new GoalsData("Learn how to issue a notification", false);
         GoalsData goal10 = new GoalsData("Add actions to notifications", false);
         GoalsData goal11 = new GoalsData("Issue notifications when certain conditions met", false);
         GoalsData goal12 = new GoalsData("Learn to put notification code off the main thread", false);
+        ArrayList<GoalsData> stagegoals3 = new ArrayList<>();
+        stagegoals3.add(goal8);
+        stagegoals3.add(goal9);
+        stagegoals3.add(goal10);
+        stagegoals3.add(goal11);
+        stagegoals3.add(goal12);
         // dummy data for road map
-        StageData stage1 = new StageData("Learn to use git", goal1, goal2, goal3, null, null);
-        StageData stage2 = new StageData("Learn about activities", goal4, goal5, goal6, goal7, null);
-        StageData stage3 = new StageData("Learn how to use Notifications", goal8, goal9, goal10, goal11, goal12);
+        StageData stage1 = new StageData("Learn to use git", stagegoals1);
+        StageData stage2 = new StageData("Learn about activities", stagegoals2);
+        StageData stage3 = new StageData("Learn how to use Notifications", stagegoals3);
         stages = new ArrayList<StageData>();
         stages.add(stage1);
         stages.add(stage2);
--- app/src/main/java/com/example/android/devmap/data/StageData.java
+++ app/src/main/java/com/example/android/devmap/data/StageData.java
@@ -6,41 +6,17 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class StageData implements Serializable{
-    private GoalsData goal1;
-    private GoalsData goal2;
-    private GoalsData goal3;
-    private GoalsData goal4;
-    private GoalsData goal5;
+    private ArrayList<GoalsData> GoalsDataArray;
+
     private String stageName;
 
-    public StageData(String a, GoalsData b, GoalsData c, GoalsData d, GoalsData e, GoalsData f) {
+    public StageData(String a, ArrayList<GoalsData> ga) {
         stageName = a;
-        goal1 = b;
-        goal2 = c;
-        goal3 = d;
-        goal4 = e;
-        goal5 = f;
+        GoalsDataArray = ga;
     }
 
     public ArrayList<GoalsData> getGoals() {
-        ArrayList<GoalsData> result = new ArrayList<>();
-        if (goal1 != null) {
-            result.add(goal1);
-        }
-        if (goal2 != null) {
-            result.add(goal2);
-        }
-        if (goal3 != null) {
-            result.add(goal3);
-        }
-        if (goal4 != null) {
-            result.add(goal4);
-        }
-        if (goal5 != null) {
-            result.add(goal5);
-        }
-
-        return result;
+        return GoalsDataArray;
 
     }
 

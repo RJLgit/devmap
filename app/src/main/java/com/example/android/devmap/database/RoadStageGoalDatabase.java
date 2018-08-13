cd0b0e4a607c6aea52114a9b4931d9ff6dcfebf8
brap <brap@brap.com>
2018-08-13 19:37:10 +0100

    0.18 added database class RoadStageGoalDatabase using the Goal, Road and Stage entities

---
+++ app/src/main/java/com/example/android/devmap/database/RoadStageGoalDatabase.java
@@ -0,0 +1,29 @@
+package com.example.android.devmap.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Goal.class, Stage.class, Road.class}, version = 1)
+public abstract class RoadStageGoalDatabase extends RoomDatabase{
+    public abstract GoalDao goalDao();
+    public abstract StageDao stageDaoDao();
+    public abstract RoadDao roadDao();
+    private static RoadStageGoalDatabase INSTANCE;
+
+    private static RoadStageGoalDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (RoadStageGoalDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            RoadStageGoalDatabase.class, "roadMap_database")
+                            .build();
+
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}

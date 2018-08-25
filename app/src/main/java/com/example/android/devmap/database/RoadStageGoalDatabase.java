b64d00cb9291d88dd6a993e8700809993398b50d
brap <doctor.fun107@gmail.com>
2018-08-25 15:44:36 +0100

    broken database

--- app/src/main/java/com/example/android/devmap/database/RoadStageGoalDatabase.java
+++ app/src/main/java/com/example/android/devmap/database/RoadStageGoalDatabase.java
@@ -20,8 +20,7 @@ public abstract class RoadStageGoalDatabase extends RoomDatabase{
     private static RoadStageGoalDatabase INSTANCE = null;
     public static Road r = new Road(0,"Front-End Web Development: Basics", "Learn the basics of front-end web development");
     public static Road r1 = new Road(1,"Back-end Web Development: Basics", "Learn the basics of back-end web development");
-    public static Road r2 = new Road(2,"Game Development: Basics", "Learn the basics of game development");
-    public static Road r3 = new Road(3,"Android Development: Basics", "Learn the basics of Android development");
+    public static Road r2 = new Road(2,"DevOps: Basics", "Learn the basics of DevOps");
 
     // RoadMap 0
     public static Stage s = new Stage(1,"HTML", "Basics of HTML", 0, "not started");
@@ -29,40 +28,69 @@ public abstract class RoadStageGoalDatabase extends RoomDatabase{
     public static Stage s2 = new Stage(3,"JavaScript", "Basics of JavaScript", 0, "not started");
     public static Stage s3 = new Stage(4,"Test yourself", "Test your knowledge", 0, "not started");
     // RoadMap 1
-    public static Stage s4 = new Stage(5,"HTML", "Basics of HTML", 1, "not started");
-    public static Stage s5 = new Stage(6,"CSS", "Basics of CSS", 1, "not started");
-    public static Stage s6 = new Stage(7,"JavaScript", "Basics of JavaScript", 1, "not started");
+    public static Stage s4 = new Stage(5,"Python", "Basics of Python", 1, "not started");
+    public static Stage s5 = new Stage(6,"Java", "Basics of Java", 1, "not started");
+    public static Stage s6 = new Stage(7,"GoLang", "Basics of Golang", 1, "not started");
     public static Stage s7 = new Stage(8,"Test yourself", "Test your knowledge", 1, "not started");
+    // RoadMap 2
+    public static Stage s8 = new Stage(9,"Programming Language", "Get some programming knowledge", 2, "not started");
+    public static Stage s9 = new Stage(10,"OS concepts", "Learn about OS concepts", 2, "not started");
+    public static Stage s10 = new Stage(11,"Servers", "Learn about managing servers", 2, "not started");
+    public static Stage s11 = new Stage(12,"Test yourself", "Test your knowledge", 2, "not started");
 
-
-    public static List<Goal> goals = new ArrayList<>();
-    public static List<Stage> stages = new ArrayList<>();
+    // RoadMAP 0
+    // stage 1
     public static Goal g1 = new Goal("Goal 1","Learn the basics and how to write semantic HTML", false, 1);
     public static Goal g2 = new Goal("Goal 2","Dividing page into sections and how to structure the DOM properly", false, 1);
     public static Goal g3 = new Goal("Goal 3","Make at least 5 HTML pages - focus on structure", false, 1);
-
+    //stage 2
     public static Goal g4 = new Goal("Goal 4","Learn the basics of CSS", false, 2);
     public static Goal g5 = new Goal("Goal 5","Learn how to use Grid and FlexBox", false, 2);
     public static Goal g6 = new Goal("Goal 6","Media Queries and Responsive websites", false, 2);
     public static Goal g7 = new Goal("Goal 7","Style the HTML Pages that you made in the last step", false, 2);
-
+    //stage 3
     public static Goal g8 = new Goal("Goal 8","Learn the syntax and basic constructs", false, 3);
     public static Goal g9 = new Goal("Goal 9","Learn how to manipulate the DOM", false, 3);
     public static Goal g10 = new Goal("Goal 10","Understand the concepts such as hoisting, event bubbling, prototype etc", false, 3);
     public static Goal g11 = new Goal("Goal 11","Learn Ajax (XHR)", false, 3);
     public static Goal g12 = new Goal("Goal 12","Learn ES6+ new features and writing modular JavaScript", false, 3);
-
+    //stage 4
     public static Goal g13 = new Goal("Goal 13","Make some responsive website and add some interactivity with JavaScript", false, 4);
     public static Goal g14 = new Goal("Goal 14","Search projects on github and open a few PRs", false, 4);
     public static Goal g15 = new Goal("Goal 15","Enhance the UI, make any demo pages responsive or improve the design", false, 4);
     public static Goal g16 = new Goal("Goal 16","Look for any open issues that you can solve", false, 4);
     public static Goal g17 = new Goal("Goal 17","Refactor any of the code or implement the best practices that you learn on the way", false, 4);
-
     // RoadMap 1
-
+    // stage 5
+    public static Goal g18 = new Goal("Goal 14","Make some responsive website and add some interactivity with JavaScript", false, 5);
+    public static Goal g19 = new Goal("Goal 15","Make some responsive website and add some interactivity with JavaScript", false, 5);
+    public static Goal g20 = new Goal("Goal 16","Make some responsive website and add some interactivity with JavaScript", false, 5);
+    // stage 6
+    public static Goal g21 = new Goal("Goal 17","Make some responsive website and add some interactivity with JavaScript", false, 6);
+    public static Goal g22 = new Goal("Goal 18","Make some responsive website and add some interactivity with JavaScript", false, 6);
+    public static Goal g23 = new Goal("Goal 19","Make some responsive website and add some interactivity with JavaScript", false, 6);
+    // stage 7
+    public static Goal g24 = new Goal("Goal 20","Make some responsive website and add some interactivity with JavaScript", false, 7);
+    public static Goal g25 = new Goal("Goal 21","Make some responsive website and add some interactivity with JavaScript", false, 7);
+    public static Goal g26 = new Goal("Goal 22","Make some responsive website and add some interactivity with JavaScript", false, 7);
+    // stage 8
+    public static Goal g27 = new Goal("Goal 23","Make some responsive website and add some interactivity with JavaScript", false, 8);
+    public static Goal g28 = new Goal("Goal 24","Make some responsive website and add some interactivity with JavaScript", false, 8);
+    public static Goal g29 = new Goal("Goal 25","Make some responsive website and add some interactivity with JavaScript", false, 8);
     // RoadMap 2
+    // stage 9
+    public static Goal g30 = new Goal("Goal 26","Make some responsive website and add some interactivity with JavaScript", false, 9);
+    public static Goal g31 = new Goal("Goal 27","Make some responsive website and add some interactivity with JavaScript", false, 9);
+    public static Goal g32 = new Goal("Goal 28","Make some responsive website and add some interactivity with JavaScript", false, 9);
+    // stage 10
+    public static Goal g33 = new Goal("Goal 29","Make some responsive website and add some interactivity with JavaScript", false, 10);
+    public static Goal g34 = new Goal("Goal 29","Make some responsive website and add some interactivity with JavaScript", false, 10);
+    public static Goal g35 = new Goal("Goal 29","Make some responsive website and add some interactivity with JavaScript", false, 10);
+    // stage 11
+    public static Goal g36 = new Goal("Goal 32","Make some responsive website and add some interactivity with JavaScript", false, 11);
+    public static Goal g37 = new Goal("Goal 32","Make some responsive website and add some interactivity with JavaScript", false, 11);
+    public static Goal g38 = new Goal("Goal 32","Make some responsive website and add some interactivity with JavaScript", false, 11);
 
-    // RoadMap 3
 
     public static RoadStageGoalDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
@@ -157,7 +185,6 @@ public abstract class RoadStageGoalDatabase extends RoomDatabase{
                 mRDao.insert(r);
                 mRDao.insert(r1);
                 mRDao.insert(r2);
-                mRDao.insert(r3);
                 // stages
                 mDao.insert(s);
                 mDao.insert(s1);
@@ -168,54 +195,60 @@ public abstract class RoadStageGoalDatabase extends RoomDatabase{
                 mDao.insert(s5);
                 mDao.insert(s6);
                 mDao.insert(s7);
-
-                // road 0 stage 0
+                mDao.insert(s8);
+                mDao.insert(s9);
+                mDao.insert(s10);
+                mDao.insert(s11);
+                // road 0 stage 1
                 mGDao.insert(g1);
                 mGDao.insert(g2);
                 mGDao.insert(g3);
-                // road 0 stage 1
+                // road 0 stage 2
                 mGDao.insert(g4);
                 mGDao.insert(g5);
                 mGDao.insert(g6);
                 mGDao.insert(g7);
-                // road 0 stage 2
+                // road 0 stage 3
                 mGDao.insert(g8);
                 mGDao.insert(g9);
                 mGDao.insert(g10);
                 mGDao.insert(g11);
                 mGDao.insert(g12);
-                // road 0 stage 3
+                // road 0 stage 4
                 mGDao.insert(g13);
                 mGDao.insert(g14);
                 mGDao.insert(g15);
                 mGDao.insert(g16);
                 mGDao.insert(g17);
-                // add road 0 stage 0 goals
-                goals.add(g1);
-                goals.add(g2);
-                goals.add(g3);
-                // add road 0 stage 1 goals
-                goals.add(g4);
-                goals.add(g5);
-                goals.add(g6);
-                goals.add(g7);
-                // add road 0 stage 2 goals
-                goals.add(g8);
-                goals.add(g9);
-                goals.add(g10);
-                goals.add(g11);
-                goals.add(g12);
-                // add road 0 stage 3 goals
-                goals.add(g13);
-                goals.add(g14);
-                goals.add(g15);
-                goals.add(g16);
-                goals.add(g17);
-                // add stages
-                stages.add(s);
-                stages.add(s1);
-                stages.add(s2);
-                stages.add(s3);
+                // road 1 stage 5
+                mGDao.insert(g18);
+                mGDao.insert(g19);
+                mGDao.insert(g20);
+                // road 1 stage 6
+                mGDao.insert(g21);
+                mGDao.insert(g22);
+                mGDao.insert(g23);
+                // road 1 stage 7
+                mGDao.insert(g24);
+                mGDao.insert(g25);
+                mGDao.insert(g26);
+                // road 1 stage 8
+                mGDao.insert(g27);
+                mGDao.insert(g28);
+                mGDao.insert(g29);
+                // road 2 stage 9
+                mGDao.insert(g30);
+                mGDao.insert(g31);
+                mGDao.insert(g32);
+                // road 2 stage 10
+                mGDao.insert(g33);
+                mGDao.insert(g34);
+                mGDao.insert(g35);
+                // road 2 stage 11
+                mGDao.insert(g36);
+                mGDao.insert(g37);
+                mGDao.insert(g38);
+
             }
 
             return null;
